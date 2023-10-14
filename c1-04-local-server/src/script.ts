718b508a06fb90f662ee82b11112423e3beb27c2
percegaroli <bruno.percegaroli@gmail.com>
2023-10-14 13:24:50 -0300

    finish local server lession

---
+++ c1-04-local-server/src/script.ts
@@ -0,0 +1,25 @@
+import { Scene, BoxGeometry, MeshBasicMaterial, Mesh, PerspectiveCamera, WebGLRenderer } from 'three';
+
+const scene = new Scene()
+
+const geometry = new BoxGeometry(1, 1, 1)
+const material = new MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new Mesh(geometry, material)
+scene.add(mesh)
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new WebGLRenderer({
+    canvas: document.querySelector('.webgl') as any
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
